f58dfe7673d7b5af9f88f8ee72e59d50ab1d3848
adnan <adgta25@gmail.com>
2022-01-19 18:05:03 +0530

    updated speed of paddle

--- paddle.py
+++ paddle.py
@@ -11,9 +11,9 @@ class Paddle(Turtle):
         self.goto(position)
 
     def up(self):
-        new_y = self.ycor() + 20
+        new_y = self.ycor() + 35
         self.goto(self.xcor(), new_y)
 
     def down(self):
-        new_y = self.ycor() - 20
+        new_y = self.ycor() - 35
         self.goto(self.xcor(), new_y)
